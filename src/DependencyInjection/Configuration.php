9ca41bea6b6020362c3d2018258be685ce3cad29
Anton Dyshkant <vyshkant@gmail.com>
2018-11-20 08:33:44 +0300

    Added DependencyInjection\Configuration class

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of VyfonyMysqldumpBundle project.
+ *
+ * (c) Anton Dyshkant <vyshkant@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Vyfony\Bundle\MysqldumpBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * @author Anton Dyshkant <vyshkant@gmail.com>
+ */
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+
+        $treeBuilder
+            ->root('vyfony_mysqldump')
+        ;
+
+        return $treeBuilder;
+    }
+}
